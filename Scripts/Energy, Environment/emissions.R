48ff88090d79b784b0df8dad18a44d0628769cb4
kevin-sang <kevin.sang@aet-int.com>
2016-12-28 10:13:07 -0500

    Emissions
    Edit emissions graph and change dataset

--- Scripts/Energy, Environment/emissions.R
+++ Scripts/Energy, Environment/emissions.R
@@ -2,12 +2,18 @@ library(ggplot2)
 library(dplyr)
 library(tidyr)
 library(readr)
-
-setwd("C:/Users/aet/Desktop/School/R projects/Final project/President-Obama-Evaluation/Data files/Energy, Environment data/CAIT_Country_GHG_Emissions_-_csv_1216/CAIT Country GHG Emissions - csv 1216")
-
-emissions <- read_csv("CAIT Country CO2 Emissions.csv", skip = 1)
+library(Cairo)
 
 setwd("C:/Users/aet/Desktop/School/R projects/Final project/President-Obama-Evaluation/Data files/Energy, Environment data")
+
+emissions <- read_csv("emissions.csv")
+emissions <- subset(emissions, emissions$`Energy Source` == "All Sources")
+
+emissions <- emissions %>%
+  group_by(Year) %>%
+  summarise(total_co2 = sum(CO2),
+            total_so2 = sum(SO2),
+            total_nox = sum(Nox))
 
 coal <- read_csv("MER_T06_02.csv")
 coal <- subset(coal, coal$YYYYMM >= 197013 & coal$Description == "Coal Consumption, Total" & coal$YYYYMM <= 201513)
@@ -18,22 +24,38 @@ coal <- coal %>%
   group_by(YYYYMM) %>%
   summarise(total = sum(Value))
 
-usa_emissions <- subset(emissions, emissions$Country == "United States" & emissions$Year >= 1970)
-
-ggplot(usa_emissions, aes(x = usa_emissions$Year, y = usa_emissions$`Total CO2 Emissions Excluding Land-Use Change and Forestry (MtCO2)`)) +
+ggplot(emissions, aes(x = Year, y = total_co2)) +
   geom_line() +
   geom_vline(xintercept = 2009) +
   xlab("Year") +
-  ylab("Total Carbon Emissions, in Metric Tons of CO2 Equivalent")
+  ylab(bquote("Total "~CO[2]~" Emissions (in metric tons)")) +
+  ggtitle("CO2") +
+  scale_y_continuous(labels = comma)
+
+ggplot(emissions, aes(x = Year, y = total_so2)) +
+  geom_line() +
+  geom_vline(xintercept = 2009) +
+  xlab("Year") +
+  ylab(bquote("Total "~SO[2]~" Emissions (in metric tons)")) +
+  ggtitle("SO2") +
+  scale_y_continuous(labels = comma)
+
+ggplot(emissions, aes(x = Year, y = total_nox)) +
+  geom_line() +
+  geom_vline(xintercept = 2009) +
+  xlab("Year") +
+  ylab(bquote("Total "~NO[x]~" Emissions (in metric tons)")) +
+  ggtitle("NOx") +
+  scale_y_continuous(labels = comma)
 
 ggplot(coal, aes(x = YYYYMM, y = total)) +
   geom_line() +
   geom_vline(xintercept = 2009) + 
   xlab("Year") +
-  ylab("Total Coal Consumption, in Thousands of Short Tons")
+  ylab("Total Coal Consumption (in thousands of short tons)")
 
 # SOURCES
 
-# Emissions data: World Resources Institute: http://www.wri.org/resources/data-sets/cait-historical-emissions-data-countries-us-states-unfccc
+# Emissions data: US Department of Energy, via Data.gov: https://catalog.data.gov/dataset/annual-1990-2011-u-s-electric-power-industry-estimated-emissions-by-state
 # Coal consumption data: US Energy Information Administration: http://www.eia.gov/coal/data.php#consumption
 
